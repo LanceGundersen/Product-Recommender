4aa59cf21178f97ca69d81cf284f94eed9e9899b
linuxlance <LanceGundersen@users.noreply.github.com>
2024-04-05 15:38:40 -0500

    adjust inversion styles

--- src/components/chatBubble/ChatBubble.tsx
+++ src/components/chatBubble/ChatBubble.tsx
@@ -34,23 +34,23 @@ export const ChatBubble = ({
   const bubbleTailClass = invertedAlignment ? 'rounded-tr-none' : 'rounded-tl-none';
 
   return (
-    <div className={`w-full ${alignmentClass}`}>
+    <div className="w-full">
       <div className="flex gap-2.5 mb-4 items-start">
-        {invertedAlignment && (
-          <img src={imgUrl} alt="Profile image" className="w-10 h-11" />
+        {!invertedAlignment && (
+          <img src={imgUrl} alt="Profile image" className="w-10 h-11 rounded-full" />
         )}
         <div className="grid">
           <div className="w-max max-w-lg">
             <div className={`px-3.5 py-2 bg-gray-100 rounded-3xl ${bubbleTailClass} items-center gap-3 inline-flex`}>
               <h5 className="text-gray-900 text-sm font-normal leading-snug">{text}</h5>
             </div>
-            <div className="justify-end items-center inline-flex mb-2.5">
+            <div className={`items-center inline-flex mb-2.5 w-full ${alignmentClass}`}>
               <h6 className="text-gray-500 text-xs font-normal leading-4 py-1">{format(dateTime, 'h:mm b')}</h6>
             </div>
           </div>
         </div>
-        {!invertedAlignment && (
-          <img src={imgUrl} alt="Profile image" className="w-10 h-11" />
+        {invertedAlignment && (
+          <img src={imgUrl} alt="Profile image" className="w-10 h-11 rounded-full" />
         )}
       </div>
     </div>
